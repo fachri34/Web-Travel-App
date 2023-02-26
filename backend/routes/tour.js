dc453e81b465efd70241c55f97092a7e786bf0bb
fachri34 <riesyafachri@gmail.com>
2023-02-26 12:02:58 +0700

    web travel app

---
+++ backend/routes/tour.js
@@ -0,0 +1,16 @@
+import express from 'express'
+import { verifyAdmin } from '../utils/verifyToken.js'
+import { createTour, deleteTour, getAllTour, getSingleTour, updateTour, getTourBySearch, getFeaturedTour, getTourCount } from './../controllers/tourController.js'
+
+const router = express.Router()
+
+router.post("/", verifyAdmin, createTour)
+router.put("/:id", verifyAdmin, updateTour)
+router.delete("/:id", verifyAdmin, deleteTour)
+router.get("/:id", getSingleTour)
+router.get("/", getAllTour)
+router.get("/search/getTourBySearch", getTourBySearch)
+router.get("/search/getFeaturedTours", getFeaturedTour)
+router.get("/search/getTourCount", getTourCount)
+
+export default router
